d3da537d51f2ce86b4a87be11e2557302c3686d4
gi-ong <pugo9165@gmail.com>
2022-12-04 19:09:23 +0900

    edited post model in insta app

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import Post 
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def dispatch(self, request, *args, **kwargs):
+        # print 비추 logger 추천
+        print("request.body: ", request.body)
+        print("request.POST: ", request.POST)
+        return super().dispatch(request, *args, **kwargs)
